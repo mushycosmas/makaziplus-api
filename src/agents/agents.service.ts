a4f6ee07a03117e291f1da6304c149b406ad3166
mushycosma <kevomassawe@gmail.com>
2026-05-28 12:11:07 +0300

    fix:

--- src/agents/agents.service.ts
+++ src/agents/agents.service.ts
@@ -22,6 +22,7 @@ export class AgentsService {
           fullName: true,
           email: true,
           role: true,
+          avator:true,
           createdAt: true,
           _count: {
             select: {
